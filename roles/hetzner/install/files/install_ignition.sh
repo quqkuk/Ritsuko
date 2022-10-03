fcf128d462c7dd5636f8e16622a008f6726ac787
quq <quq@magisystem.xyz>
2022-10-03 15:15:52 +0200

    Simplified install script due to the disk being bigger than 20GB

--- roles/hetzner/install/files/install_ignition.sh
+++ roles/hetzner/install/files/install_ignition.sh
@@ -13,34 +13,12 @@ rm "$TMPIMG"
 # Move GPT trailer to end of disk
 sgdisk -e "$1"
 
-# Find Root Partition
-PARTED_OUTPUT=$(parted -s "$1" unit MiB print)
-ROOT_PARTITION=$(printf "$PARTED_OUTPUT" | grep 'p.lxroot' | tr -s ' ')
-ROOT_PART_NUMBER=$(printf "$ROOT_PARTITION" | cut -d ' ' -f 2)
-SHRINK_FACTOR_MiB=$((2*1024))
-
-# Shrink btrfs filesystem
-mount "$1$ROOT_PART_NUMBER" /mnt
-btrfs filesystem resize --enqueue "-${SHRINK_FACTOR_MiB}M" /mnt
-umount /mnt
-
-# Shrink partition
-ROOT_PART_END=$(printf "$ROOT_PARTITION" | cut -d ' ' -f 4  | tr -d 'MiB')
-ROOT_PART_NEW_END="$((ROOT_PART_END - SHRINK_FACTOR_MiB))"
-parted "$1" ---pretend-input-tty resizepart "$ROOT_PART_NUMBER" "${ROOT_PART_NEW_END}MiB" <<< "Yes"
-
-#TODO: This script is dependant on the disk being 20GB, that might not be true
-#      but as of right now it's a safe assumption as we don't deploy on larger disks
-#Move var/spare partition and create swap and ignition config partitions
-printf "%iM\n" "-${SHRINK_FACTOR_MiB}" | sfdisk --move-data "$1" -N "$(printf "$PARTED_OUTPUT" | grep 'p.spare' | tr -s ' ' | cut -d ' ' -f 2)"
-partprobe "$1" && udevadm settle
-
 #Create swap and ignition config partitions
 PARTED_OUTPUT=$(parted -s "$1" unit MiB print)
-LAST_PART_END=$(printf "$PARTED_OUTPUT" | tail -n1 | tr -s ' ' | cut -d ' ' -f4 | tr -d 'MiB')
 DISK_END=$(printf "$PARTED_OUTPUT" | grep "Disk $1" | cut -d ' ' -f3 | tr -d 'MiB')
+SWAP_START=$((DISK_END-2*1024))
 SWAP_END=$((DISK_END-512))
-parted -s "$1" mkpart primary linux-swap "${LAST_PART_END}MiB" "${SWAP_END}MiB"
+parted -s "$1" mkpart primary linux-swap "${SWAP_START}MiB" "${SWAP_END}MiB"
 parted -s "$1" mkpart primary ext4 "${SWAP_END}MiB" 100%
 partprobe "$1" && udevadm settle
 
