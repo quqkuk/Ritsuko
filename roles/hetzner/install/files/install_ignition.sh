5f12e521a5432e44c3e29beb9e561823e8ec6533
quq <quq@magisystem.xyz>
2022-11-27 22:21:27 +0100

    Moved to Leap Micro

--- roles/hetzner/install/files/install_ignition.sh
+++ roles/hetzner/install/files/install_ignition.sh
@@ -5,13 +5,18 @@
 set -ex
 
 # Download image and write it to disk
-TMPIMG=$(mktemp --suffix=".qcow2")
-curl -sL https://mirrorcache.opensuse.org/tumbleweed/appliances/openSUSE-MicroOS.x86_64-ContainerHost-kvm-and-xen.qcow2 > "$TMPIMG"
-qemu-img convert -f qcow2 -O host_device "$TMPIMG" "$1"
-rm "$TMPIMG"
+#TMPIMG=$(mktemp --suffix=".qcow2")
+#curl -sL https://mirrorcache.opensuse.org/tumbleweed/appliances/openSUSE-MicroOS.x86_64-ContainerHost-kvm-and-xen.qcow2 > "$TMPIMG"
+#qemu-img convert -f qcow2 -O host_device "$TMPIMG" "$1"
+#rm "$TMPIMG"
+curl -sL 'https://download.opensuse.org/distribution/leap-micro/5.3/appliances/openSUSE-Leap-Micro.x86_64-Default.raw.xz' | \
+	xz --decompress > "$1"
 
 # Move GPT trailer to end of disk
-sgdisk -e "$1"
+sfdisk --relocate gpt-bak-std "$1"
+
+# Update partition table
+partprobe "$1" && udevadm settle
 
 #Create swap and ignition config partitions
 PARTED_OUTPUT=$(parted -s "$1" unit MiB print)
@@ -22,5 +27,5 @@ parted -s "$1" mkpart primary linux-swap "${SWAP_START}MiB" "${SWAP_END}MiB"
 parted -s "$1" mkpart primary ext4 "${SWAP_END}MiB" 100%
 partprobe "$1" && udevadm settle
 
-#TODO: We can rely on this being the 5th partition for now, but we probably shouldn't
-mkswap "${1}5"
+#TODO: We can rely on this being the 4th partition for now, but we probably shouldn't
+mkswap "${1}4"
